e4010e8af4d9eeac1a1c1606dad213ece1ef68c4
Crowdbotics <engineering@crowdbotics.com>
2022-08-31 07:57:43 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/gtyud_dev_31036/MainActivity.java
@@ -0,0 +1,15 @@
+package com.gtyud_dev_31036;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "gtyud_dev_31036";
+  }
+}
